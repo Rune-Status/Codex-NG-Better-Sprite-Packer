9689b1dfd38f53d3d6ec6539bdf475af758a4241
Chad <nshusa99@gmail.com>
2017-06-15 02:46:31 -0500

    Implemented the pack method

--- src/io/creativelab/Controller.java
+++ src/io/creativelab/Controller.java
@@ -1,13 +1,19 @@
 package io.creativelab;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.creativelab.sprite.SpriteCache;
+import io.creativelab.util.Dialogue;
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
 public final class Controller implements Initializable {
@@ -23,6 +29,38 @@ public final class Controller implements Initializable {
 	
 	@FXML
 	private void pack() {
+		DirectoryChooser chooser = new DirectoryChooser();
+		chooser.setTitle("Select the directory that contains your sprites.");
+		chooser.setInitialDirectory(new File(System.getProperty("user.home")));
+		
+		File selectedDirectory = chooser.showDialog(App.getMainStage());
+
+		if (selectedDirectory == null) {
+			return;
+		}
+		
+		new Thread(new Task<Boolean>() {
+
+			@Override
+			protected Boolean call() throws Exception {
+				try {
+					SpriteCache cache = SpriteCache.load(selectedDirectory);
+					
+					try(FileOutputStream fos = new FileOutputStream("./main_file_sprites.dat")) {
+						fos.write(cache.encode());
+					}
+					
+					Platform.runLater(() -> {
+						Dialogue.openDirectory("Would you like to view this file?", new File("./"));
+					});					
+				} catch (IOException e) {
+					Platform.runLater(() -> {
+						Dialogue.showException("An error occurred", e).showAndWait();
+					});					
+				}
+				return true;
+			}			
+		}).start();
 		
 	}
 	
